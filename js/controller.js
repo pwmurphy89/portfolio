9d9bb473645cfcb4685bdfd3ad067ee6645cf1cc
pwmurphy89 <pwmurphy89@gmail.com>
2016-05-13 09:03:46 -0400

    updated urls for aws

--- js/controller.js
+++ js/controller.js
@@ -2,13 +2,16 @@ var myApp = angular.module("myApp",['ngRoute']);
 
 myApp.config(function($routeProvider, $locationProvider){
 	$routeProvider.when('/', {
-		templateUrl: 'views/front.html',
+		templateUrl: 'portfolio/views/front.html',
 		controller: 'myController'
 	}).when('/technologies',{
-		templateUrl: 'views/technologies',
+		templateUrl: 'portfolio/views/technologies',
 		controller: 'myController'
 	}).when('/projects',{
-		templateUrl: 'views/projects',
+		templateUrl: 'portfolio/views/projects',
+		controller: 'myController'
+	}).when('/contact',{
+		templateUrl: 'portfolio/views/contact',
 		controller: 'myController'
 	})
 });
